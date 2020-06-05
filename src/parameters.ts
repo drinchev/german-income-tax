2f905a8b6e3f3f2272c308532e1ca2d31ef10454
Jay <jayasurian123@yahoo.com>
2020-06-05 17:02:04 +0200

    added parameters for year 2020

--- src/parameters.ts
+++ src/parameters.ts
@@ -18,7 +18,8 @@ export enum Year {
     Y2016 = 2016,
     Y2017 = 2017,
     Y2018 = 2018,
-    Y2019 = 2019
+    Y2019 = 2019,
+    Y2020 = 2020
 }
 
 /**
@@ -133,6 +134,27 @@ export default {
         progression : {
             [Zone.Second] : 980.14,
             [Zone.Third] : 216.16
+        },
+        percent : {
+            [Zone.Fourth] : 0.42,
+            [Zone.Fifth] : 0.45,
+        },
+        solidarity : 0.055,
+        solidarityThreshold : 972
+    },
+
+    /**
+     * 2020
+     */
+    [Year.Y2020] : {
+        zoneBoundaries : [0, 9408, 14532, 57051, 270500, Infinity],
+        threshold : {
+            [Zone.Second] : 0.14,
+            [Zone.Third] : 0.2397
+        },
+        progression : {
+            [Zone.Second] : 972.87,
+            [Zone.Third] : 212.02
         },
         percent : {
             [Zone.Fourth] : 0.42,
